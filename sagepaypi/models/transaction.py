1c91dca08a8039b07142a25f9d224b74743eaca9
smiths0n <alex@smiths0n.co.uk>
2022-08-31 18:21:04 +0100

    Update transaction.py

--- sagepaypi/models/transaction.py
+++ sagepaypi/models/transaction.py
@@ -8,7 +8,7 @@ from django.db import models
 from django.db.models.manager import BaseManager
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 import pycountry
 
